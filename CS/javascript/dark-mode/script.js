582cb0a939d225eaf2536ae24fe8460afafd95c6
makhai865 <makhai.harmon@hgs.hiddengeniusproject.org>
2024-09-17 18:57:32 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,9 @@
 function darkMode() {
-// Your code here.
+    if (document.body.style.backgroundColor === "white") {
+        document.body.style.backgroundColor = "blue";
+        document.body.style.color = "white";
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    }
 }
