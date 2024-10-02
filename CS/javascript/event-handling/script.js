81b2a6d673273e2999a67c59c184643ec4d17856
makhai865 <makhai.harmon@hgs.hiddengeniusproject.org>
2024-10-01 18:54:45 -0700

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,6 @@
 function eventHandling() {
-    // Your code here.
-}
+document.getElementById("clickme").addEventListener('click', function() {
+    image = document.createElement('img');
+    image.src = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+    document.body.appendChild(image);
+});}
